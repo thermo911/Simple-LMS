1688c8a08da1f0b84b26638897fa9802c28f1b92
Alexander Blazhkov <thermo911@yandex.ru>
2021-07-12 12:23:08 +0300

    Add handling of course creation and course validation

--- Spring_SandBox/src/main/java/com/blazhkov/demo/controller/CourseController.java
+++ Spring_SandBox/src/main/java/com/blazhkov/demo/controller/CourseController.java
@@ -1,12 +1,17 @@
 package com.blazhkov.demo.controller;
 
 import com.blazhkov.demo.dao.CourseRepository;
+import com.blazhkov.demo.domain.Course;
+import com.blazhkov.demo.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/course")
@@ -25,10 +30,40 @@ public class CourseController {
         return "course_table";
     }
 
-    @GetMapping("/{id}")
+    @RequestMapping("/{id}")
     public String courseForm(Model model, @PathVariable("id") Long id) {
-        System.out.println(id);
-        model.addAttribute("course", courseRepository.findById(id).get());
+        model.addAttribute("course", courseRepository.findById(id)
+                .orElseThrow(NotFoundException::new));
         return "course_form";
+    }
+
+    @PostMapping
+    public String submitCourseForm(@Valid Course course, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "course_form";
+        }
+        courseRepository.save(course);
+        return "redirect:/course";
+    }
+
+    @RequestMapping("/new")
+    public String courseForm(Model model) {
+        model.addAttribute("course", new Course());
+        return "course_form";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCourse(@PathVariable("id") Long id) {
+        courseRepository.delete(id);
+        return "redirect:/course";
+    }
+
+    /* Exception handlers */
+
+    @ExceptionHandler
+    public ModelAndView notFoundExceptionHandler(NotFoundException ex) {
+        ModelAndView modelAndView = new ModelAndView("course_not_found");
+        modelAndView.setStatus(HttpStatus.NOT_FOUND);
+        return modelAndView;
     }
 }
