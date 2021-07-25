b9c804a3059a9ee66481cf8290bd48a9cf82379a
Alexander Blazhkov <thermo911@yandex.ru>
2021-07-25 11:31:58 +0300

    Add database blueprints

---
+++ Spring_SandBox/src/main/resources/sql/indices.sql
@@ -0,0 +1,5 @@
+CREATE INDEX user_names      ON users    (name);
+CREATE INDEX user_surnames   ON users    (surname);
+CREATE INDEX user_nicknames  ON users    (nickname);
+CREATE INDEX course_titles   ON courses  (title);
+CREATE INDEX category_titles on category (title);
