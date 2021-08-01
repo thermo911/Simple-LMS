9f82a409655431debbe8780477239f42615ea4f0
Alexander Blazhkov <thermo911@yandex.ru>
2021-08-01 19:00:21 +0300

    Add responding with HTTP-403 code

--- Spring_SandBox/src/main/java/com/blazhkov/demo/controller/AuthController.java
+++ Spring_SandBox/src/main/java/com/blazhkov/demo/controller/AuthController.java
@@ -1,13 +1,17 @@
 package com.blazhkov.demo.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AuthController {
 
     @RequestMapping("/access_denied")
-    public String accessDenied() {
-        return "access_denied";
+    public ModelAndView accessDenied() {
+        ModelAndView modelAndView = new ModelAndView("access_denied");
+        modelAndView.setStatus(HttpStatus.FORBIDDEN);
+        return modelAndView;
     }
 }
